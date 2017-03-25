ade63bc5a7bd424c0cb48377834b78dda6e16a3c
MrTing <mrctwu@gmail.com>
2017-03-25 10:38:58 -0500

    Finished Data Copy

--- student-distrib/file_system.c
+++ student-distrib/file_system.c
@@ -27,13 +27,17 @@ int32_t file_write(int32_t fd, void8 buf, int32_t nbytes){
 }
 
 int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t length){
+  int32_t n;
   int32_t data_length;
+  int32_t data_copied;
   int32_t data_need_read;
   int32_t data_bl_start;
   int32_t index = 0;
   uint32_t current_block;
+  inode_t* inode_copy;
+  uint8_t* byte_copy;
 
-
+  inode_copy = (first_inode + inode);
   data_length = (first_inode + inode)->inode_len;
   data_length -= offset;      // Include the offset and get the actual length of data
 
@@ -54,7 +58,20 @@ int32_t read_data (uint32_t inode, uint32_t offset, uint8_t* buf, uint32_t lengt
     data_need_read = length;
   }
 
+  data_copied = 0;
   while (data_need_read > 0){
-    buf[index]
+    current_block = offset / DATA_B_SIZE;
+
+    byte_copy = (uint8_t*) &data[inode_copy->in_d_block_num[current_block]] + (offset % DATA_B_SIZE);
+
+    n = (length > 4096 - (offset % 4096))?(4096 - (offset % 4096))):length;
+    memcpy(buf, byte_copy, n);
+
+    data_need_read -= n;
+    data_copied += n;
+    buf += n;
+    offset += n;
+    }
+    return data_copied;
   }
 }
